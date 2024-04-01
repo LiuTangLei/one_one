f11dbd78393acd0ddfca61cc785eaa05704493b2
liutanglei <liu.tang.lei@gmail.com>
2024-04-01 14:55:58 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.one_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:one_one/app/app.dart';
+import 'package:one_one/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
